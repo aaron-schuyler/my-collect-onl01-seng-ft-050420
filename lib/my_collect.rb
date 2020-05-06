01b322b50c5197d7ae45f49183038c896236492c
Aaron Schuyler <asky28@gmail.com>
2020-05-06 19:14:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   modified = []
   i = 0
   while i < collection.length do
-    modified.push(yeild(collection[i]))
+    modified.push(yield(collection[i]))
   end
   modified
 end
